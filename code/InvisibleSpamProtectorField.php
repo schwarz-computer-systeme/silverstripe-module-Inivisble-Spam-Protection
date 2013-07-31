4c8d568118620497d1d84dab3e6a4e445f2c0fa9
Kevin May <km@schwarz.de>
2013-07-31 15:13:10 +0200

    changed CSS into css

--- code/InvisibleSpamProtectorField.php
+++ code/InvisibleSpamProtectorField.php
@@ -35,7 +35,7 @@ class InvisibleSpamProtectorField extends SpamProtectorField {
 	 * @return HTML
 	 */
 	function Field($properties = array()) {
-		Requirements::CSS("css/InvisibleSpamProtector.css");
+		Requirements::css("css/InvisibleSpamProtector.css");
 
 		if(self::showAntiSpam()) {
 			$attributes = array(
